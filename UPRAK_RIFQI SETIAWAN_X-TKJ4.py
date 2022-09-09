6c11b319c8673b1eb13e321f86387f34f5095443
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-09-09 14:35:05 +0700

    Add files via upload

---
+++ UPRAK_RIFQI SETIAWAN_X-TKJ4.py
@@ -0,0 +1,38 @@
+# balok panjang * lebar * tinggi
+
+panjang = 30
+lebar = 20
+tinggi = 10
+balok = panjang * lebar * tinggi
+print("luas dari balok adalah",balok)
+
+# kubus sisi * sisi * sisi
+
+sisi = 20
+sisi = 20
+sisi = 20
+kubus = sisi * sisi * sisi
+print("luas dari kubus adalah",kubus)
+
+# limas = 1/3 * luas_alas * tinggi
+
+luas_alas = 20
+tinggi = 30
+limas = 1/3 * luas_alas * tinggi
+print("luas dari limas adalah",limas)
+
+# tabung 22/7 * r * r * t
+
+r = 30
+t = 10
+tabung = 22/7 * r * r * t
+print("luas dari tabung adalah",tabung)
+
+# celcis to remaur = 5/4
+
+celcius = 1000
+remaur = 0.8
+remaur = celcius * remaur
+print("celcius ke remaur",remaur)
+
+
